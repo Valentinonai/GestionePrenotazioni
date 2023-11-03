cf13069a374ce8e803033818eb6dbfee4cbe5f13
Valentinonai <137069763+Valentinonai@users.noreply.github.com>
2023-11-03 13:34:36 +0100

    Initial commit

---
+++ src/test/java/Esercizio3112023/GestionePrenotazioni/GestionePrenotazioniApplicationTests.java
@@ -0,0 +1,13 @@
+package Esercizio3112023.GestionePrenotazioni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionePrenotazioniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
